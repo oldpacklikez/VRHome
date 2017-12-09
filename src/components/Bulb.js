d8c4ec9bdf303b2b934177abb9ae8c7db6ef1102
packlikez <p-ing@windowslive.com>
2017-12-09 22:15:21 +0700

    all set

--- src/components/Bulb.js
+++ src/components/Bulb.js
@@ -1,11 +1,11 @@
 import 'aframe';
 import 'babel-polyfill';
-import { Entity, Scene } from 'aframe-react';
+import { Entity } from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 class Bulb extends React.Component {
   constructor(props) {
-    super(props); 
+    super(props);
     this.state = {
       bulb: false
     };
@@ -29,77 +29,57 @@ class Bulb extends React.Component {
   fuse() {
     console.log('fusing')
   }
+
+
   render() {
     return (
-      <div>
-      <Entity events={{
-        click: this.changeBulb.bind(this),
-        fusing: this.fuse.bind(this)
-      }}>
+      <Entity>
+        {/* ON panel */}
+        <Entity primitive="a-plane"
+          height="1" width="1"
+          rotation="0 -15 0"
+          color={
+            this.state.bulb ? 'green' : 'white'
+          }
+          position={{ x: 3, y: 4, z: -4 }}
+          events={{
+            click: this.bulbOn.bind(this)
+          }}>
+          <Entity text={{
+            value: 'On',
+            color: 'black',
+            width: 4,
+            align: 'center'
+          }} />
+        </Entity>
+
+        {/* OFF panel */}
+        <Entity primitive="a-plane"
+          height="1"
+          width="1"
+          rotation="0 -15 0"
+          color={
+            this.state.bulb ? 'white' : 'red'
+          }
+          position={{ x: 4, y: 4, z: -3.7 }}
+          events={{
+            click: this.bulbOff.bind(this)
+          }}>>
+                                                        <Entity text={{
+            value: 'Off',
+            color: 'black',
+            width: 4,
+            align: 'center'
+          }} />
+        </Entity>
+
+        {/* Bulb model */}
         <Entity gltf-model={this.state.bulb ? "#on" : "#off"}
           position={{ x: 0, y: 4, z: -3 }}
           scale={{ x: 0.25, y: 0.25, z: 0.25 }}
-
         />
-      </Entity >
-      <Entity
-        primitive="a-plane"
-        height="1"
-        width="1"
-        rotation="0 -15 0"
-        color={this.state.bulb ? 'green' : 'white'}
-        position={{ x: 3, y: 4, z: -4 }}
-        events={{
-          click: this.bulbOn.bind(this)
-        }}>
-        <Entity text={{ 
-          value: 'On', 
-          color: 'black',
-          width: 4, 
-          align: 'center' }} />
       </Entity>
-
-      <Entity 
-      primitive="a-plane" 
-      height="1" 
-      width="1" 
-      rotation="0 -15 0" 
-      color={this.state.bulb ? 'white' : 'red'} 
-      position={{ x: 4, y: 4, z: -3.7 }}
-        events={{
-          click: this.bulbOff.bind(this)
-        }}>>
-         <Entity text={{ 
-           value: 'Off', 
-           color: 'black', 
-           width: 4, 
-           align: 'center' }} />
-      </Entity>
-      <Entity
-       primitive="a-plane" 
-       height="1" 
-       width="1" 
-       rotation="0 -15 0" 
-       color={this.state.bulb ? 'green' : 'white'} 
-       position={{ x: 3, y: 4, z: -4 }}
-        events={{
-          click: this.bulbOn.bind(this)
-        }}>
-        <Entity text={{ 
-          alue: 'On',
-           color: 'black', 
-           width: 4, 
-           align: 'center' }} />
-      </Entity>
-
-      <Entity primitive="a-plane" height="1" width="1" rotation="0 -15 0" color={this.state.bulb ? 'white' : 'red'} position={{ x: 4, y: 4, z: -3.7 }}
-        events={{
-          click: this.bulbOff.bind(this)
-        }}>>
-     <Entity text={{ value: 'Off', color: 'black', width: 4, align: 'center' }} />
-      </Entity>
-      </div>
-      );
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import 'babel-polyfill';
 import { Entity, Scene } from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Bulb from './components/Bulb'
+import Bulb from './components/Bulb';
+import Box from './components/Box';
+import Environment from './components/Environment';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -13,27 +16,9 @@ class App extends React.Component {
     };
 
   }
-
- changeBulb() {
-    this.setState({
-      bulb: !(this.state.bulb)
-    });
-    console.log('Change bulb ' + this.state.bulb)
+  daydreamClick(){
+    alert('Hello world!');
   }
-  bulbOn() {
-    this.setState({
-      bulb: true
-    })
-  }
-  bulbOff() {
-    this.setState({
-      bulb: false
-    })
-  }
-  fuse() {
-    console.log('fusing')
-  }
-
 
   render() {
     return (
@@ -41,41 +26,22 @@ class App extends React.Component {
         <a-assets>
           <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" />
           <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" />
-          <a-asset-item id="off" src="/model/scene.gltf" />
+         <a-asset-item id="off" src="/model/scene.gltf" />
           <a-asset-item id="on" src="/model/bulb_open.gltf" />
         </a-assets>
-
-        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100" />
-        <Entity primitive="a-light" type="ambient" color="#445451" />
-        <Entity primitive="a-light" type="point" intensity="2" position="2 4 4" />
-        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048" />
-
-        <Entity primitive="a-plane" height="1" width="1" rotation="0 -15 0" color={this.state.bulb?'green':'white'} position={{ x: 3, y: 4, z: -4 }}
-          events={{
-            click: this.bulbOn.bind(this)
-          }}>
-          <Entity text={{ value: 'On', color: 'black', width: 4, align: 'center' }} />
-        </Entity>
-
-        <Entity primitive="a-plane" height="1" width="1" rotation="0 -15 0" color={this.state.bulb?'white':'red'} position={{ x: 4, y: 4, z: -3.7 }}
-          events={{
-            click: this.bulbOff.bind(this)
-          }}>>
-        <Entity text={{ value: 'Off', color: 'black', width: 4, align: 'center' }} />
-        </Entity>
-        <Entity daydream-controls="hand: left"></Entity>
-        <Entity gltf-model={this.state.bulb ? "#on" : "#off"}
-          position={{ x: 0, y: 4, z: -3 }}
-          scale={{ x: 0.25, y: 0.25, z: 0.25 }}
-
-        />
-  
+       <VoiceRecognitionDemo/>
+       <Environment/>
+        <Bulb/>
+        <a-entity laser-controls line="color: red; opacity: 1"></a-entity>
+       
         <Entity primitive="a-camera" camera={{ userHeight: 1.6 }}>
-          <a-entity cursor="fuse: true; fuseTimeout: 500"
-            position="0 0 -1"
+          <a-entity cursor="fuse: false; fuseTimeout: 500"
+          /*  position="0 0 -1"
             geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
-            material="color: black; shader: flat">
+    material="color: black; shader: flat">*/>
           </a-entity>
+         
+        
         </Entity>
       </Scene>
     );
